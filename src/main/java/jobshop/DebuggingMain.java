8d3054a3cf67902504e00f52a3e7866ba7e1e4c3
Lucas Perard <perardlucas@gmail.com>
2020-04-13 17:29:53 +0200

    cloned repo

--- src/main/java/jobshop/DebuggingMain.java
+++ src/main/java/jobshop/DebuggingMain.java
@@ -1,8 +1,6 @@
 package jobshop;
 
 import jobshop.encodings.JobNumbers;
-import jobshop.encodings.ResourceOrder;
-import jobshop.encodings.Task;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -26,24 +24,14 @@ public class DebuggingMain {
             enc.jobs[enc.nextToSet++] = 0;
             enc.jobs[enc.nextToSet++] = 1;
 
-            ResourceOrder ro = new ResourceOrder(instance);
-            ro.rep[0][0]=new Task(0,0);
-            ro.rep[0][1]=new Task(1,1);
-            ro.rep[1][0]=new Task(1,0);
-            ro.rep[1][1]=new Task(0,1);
-            ro.rep[2][0]=new Task(0,2);
-            ro.rep[2][1]=new Task(1,2);
-
-
-            System.out.println("\nENCODING: " + ro.toString());
             System.out.println("\nENCODING: " + enc);
 
             Schedule sched = enc.toSchedule();
+            // TODO: make it print something meaningful
             // by implementing the toString() method
             System.out.println("SCHEDULE: " + sched);
             System.out.println("VALID: " + sched.isValid());
             System.out.println("MAKESPAN: " + sched.makespan());
-            System.out.println("Times: " + sched.toString());
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/jobshop/encodings/ResourceOrder.java
+++ src/main/java/jobshop/encodings/ResourceOrder.java
@@ -4,87 +4,128 @@ import jobshop.Encoding;
 import jobshop.Instance;
 import jobshop.Schedule;
 
-import java.util.Arrays;
-
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.IntStream;
 
 public class ResourceOrder extends Encoding {
 
+    // for each machine m, taskByMachine[m] is an array of tasks to be
+    // executed on this machine in the same order
+    public final Task[][] tasksByMachine;
 
-    public final Task[][] rep;
+    // for each machine, indicate on many tasks have been initialized
+    public final int[] nextFreeSlot;
 
-    public ResourceOrder(Instance instance) {
+    /** Creates a new empty resource order. */
+    public ResourceOrder(Instance instance)
+    {
         super(instance);
-        rep = new Task[instance.numMachines][instance.numJobs];
 
-        for (int i = 0; i < this.rep.length; i++) {
-            for (int j = 0; j < this.rep[i].length; j++) {
-                rep[i][j] = new Task(-1, -1);
-            }
+        // matrix of null elements (null is the default value of objects)
+        tasksByMachine = new Task[instance.numMachines][instance.numJobs];
+
+        // no task scheduled on any machine (0 is the default value)
+        nextFreeSlot = new int[instance.numMachines];
+    }
+
+    /** Creates a resource order from a schedule. */
+    public ResourceOrder(Schedule schedule)
+    {
+        super(schedule.pb);
+        Instance pb = schedule.pb;
+
+        this.tasksByMachine = new Task[pb.numMachines][];
+        this.nextFreeSlot = new int[instance.numMachines];
+
+        for(int m = 0 ; m<schedule.pb.numMachines ; m++) {
+            final int machine = m;
+
+            // for thi machine, find all tasks that are executed on it and sort them by their start time
+            tasksByMachine[m] =
+                    IntStream.range(0, pb.numJobs) // all job numbers
+                            .mapToObj(j -> new Task(j, pb.task_with_machine(j, machine))) // all tasks on this machine (one per job)
+                            .sorted(Comparator.comparing(t -> schedule.startTime(t.job, t.task))) // sorted by start time
+                            .toArray(Task[]::new); // as new array and store in tasksByMachine
+
+            // indicate that all tasks have been initialized for machine m
+            nextFreeSlot[m] = instance.numJobs;
         }
     }
 
-    public boolean finished (int[]  nextTask) {
-        for(int i : nextTask){
-            if ( i<instance.numTasks){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public Task nextCouple(int res, Task curCouple){
-        for(int i=0;i<(instance.numJobs-1);i++){
-            if(rep[res][i].equals(curCouple)){
-                return rep[res][i+1];
-            }
-        }
-        Task nullTask = new Task(-1,-1);
-        return nullTask;
-    }
     @Override
     public Schedule toSchedule() {
-        // time at which each machine is going to be freed
-        int[] nextFreeTimeResource = new int[instance.numMachines];
+        // indicate for each task that have been scheduled, its start time
+        int [][] startTimes = new int [instance.numJobs][instance.numTasks];
 
-        // for eachS job, the first task that has not yet been scheduled
-        int[] nextTask = new int[instance.numJobs];
-        int[] nextFreeTimeJob = new int[instance.numJobs];
-        for (int j =0;j<instance.numJobs;j++){
-            nextTask[j]=0;
-            nextFreeTimeJob[j]=0;
-        }
+        // for each job, how many tasks have been scheduled (0 initially)
+        int[] nextToScheduleByJob = new int[instance.numJobs];
 
-        Task[] nextCouple = new Task[instance.numMachines];
+        // for each machine, how many tasks have been scheduled (0 initially)
+        int[] nextToScheduleByMachine = new int[instance.numMachines];
 
-        for(int r =0;r< instance.numMachines;r++){
-            nextCouple[r]=rep[r][0];
-            nextFreeTimeResource[r]=0;
-        }
+        // for each machine, earliest time at which the machine can be used
+        int[] releaseTimeOfMachine = new int[instance.numMachines];
 
 
-        // for each task, its start time
-        int[][] startTimes = new int[instance.numJobs][instance.numTasks];
-        int time =0;
-        while (!finished(nextTask)){
-            for(int job=0;job<instance.numJobs;job++){
-                int task = nextTask[job];
-                if (task <instance.numTasks){
-                    int res =instance.machine(job, task);
-                    Task cur = new Task(job, task);
-                    //System.out.println("check cur "+ cur.toString()+" equals "+nextCouple[res].toString());
-                    if (nextCouple[res].equals(cur)){
-                        time=Math.max(nextFreeTimeResource[res], nextFreeTimeJob[job]);
-                        startTimes[job][task]=time;
-                        nextFreeTimeResource[res]=time + instance.duration(job, task);
-                        nextFreeTimeJob[job]=time + instance.duration(job, task);
-                        nextTask[job]+=1;
-                        nextCouple[res]=nextCouple(res,cur);
-                    }
-                }
+        // loop while there remains a job that has unscheduled tasks
+        while(IntStream.range(0, instance.numJobs).anyMatch(m -> nextToScheduleByJob[m] < instance.numTasks)) {
+
+            // selects a task that has noun scheduled predecessor on its job and machine :
+            //  - it is the next to be schedule on a machine
+            //  - it is the next to be scheduled on its job
+            // if there is no such task, we have cyclic dependency and the solution is invalid
+            Optional<Task> schedulable =
+                    IntStream.range(0, instance.numMachines) // all machines ...
+                    .filter(m -> nextToScheduleByMachine[m] < instance.numJobs) // ... with unscheduled jobs
+                    .mapToObj(m -> this.tasksByMachine[m][nextToScheduleByMachine[m]]) // tasks that are next to schedule on a machine ...
+                    .filter(task -> task.task == nextToScheduleByJob[task.job])  // ... and on their job
+                    .findFirst(); // select the first one if any
+
+            if(schedulable.isPresent()) {
+                // we found a schedulable task, lets call it t
+                Task t = schedulable.get();
+                int machine = instance.machine(t.job, t.task);
+
+                // compute the earliest start time (est) of the task
+                int est = t.task == 0 ? 0 : startTimes[t.job][t.task-1] + instance.duration(t.job, t.task-1);
+                est = Math.max(est, releaseTimeOfMachine[instance.machine(t)]);
+                startTimes[t.job][t.task] = est;
+
+                // mark the task as scheduled
+                nextToScheduleByJob[t.job]++;
+                nextToScheduleByMachine[machine]++;
+                // increase the release time of the machine
+                releaseTimeOfMachine[machine] = est + instance.duration(t.job, t.task);
+            } else {
+                // no tasks are schedulable, there is no solution for this resource ordering
+                return null;
             }
         }
-
+        // we exited the loop : all tasks have been scheduled successfully
         return new Schedule(instance, startTimes);
-
     }
+
+    /** Creates an exact copy of this resource order. */
+    public ResourceOrder copy() {
+        return new ResourceOrder(this.toSchedule());
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder s = new StringBuilder();
+        for(int m=0; m < instance.numMachines; m++)
+        {
+            s.append("Machine ").append(m).append(" : ");
+            for(int j=0; j<instance.numJobs; j++)
+            {
+                s.append(tasksByMachine[m][j]).append(" ; ");
+            }
+            s.append("\n");
+        }
+
+        return s.toString();
+    }
+
 }
