0d97bc45614a7bea3d6958ec844cfcea68d12113
Lucas Perard <perardlucas@gmail.com>
2020-04-14 15:20:47 +0200

    now project can begin

---
+++ src/main/java/jobshop/solvers/GreedySolver.java
@@ -0,0 +1,11 @@
+package jobshop.solvers;
+
+import jobshop.Solver;
+import jobshop.Schedule;
+
+public class GreedySolver implements Solver {
+    @Override
+    public Schedule toSchedule() {
+    return null;
+    }
+}
